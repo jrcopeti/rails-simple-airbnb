9cfa1c94a5a9f47dd285e14e49616a5b5a87283c
Jose Copeti <jrcopeti@gmail.com>
2023-01-27 17:20:02 +0100

    challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Flat.destroy_all
+
+puts 'creating flats'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+)
+
+10.times do
+  Flat.create!(
+    name: Faker::Games::Zelda.game,
+    address: Faker::Address.street_address,
+    description: Faker::Hipster.paragraph,
+    price_per_night: rand(50..300),
+    number_of_guests: rand(1..5),
+    picture_url: 'https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  )
+  puts 'created tudo'
+end
+
+puts 'flats created successfully'
